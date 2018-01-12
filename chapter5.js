e571dd4f1612bfe9543acda8ec2c04bf9ae10887
strayer89 <kangzhitong89@gmail.com>
2018-01-12 23:08:37 +0800

    update

--- chapter5.js
+++ chapter5.js
@@ -1,6 +1,29 @@
 第五章 引用类型
+	* 引用类型的值（对象）是引用类型的一个实例。
+ 	* 引用类型也被称为对象定义，它们描述的是一类对象所具有的属性和方法；
+ 	* 构造函数（Object()）本身就是一个函数，它们的目的是出于创建新对象；
+ 	* var person = new Object();
  5.1 Object 类型
- 	* 引用类型的值是引用类型的一个实例。
+ 	* 两种创建方法
+ 		1> new 操作符创建
+ 			var person = new Object();
+ 		    person.name = "kang";
+ 		    person.age = 22;
+ 		2> 字面量创建
+ 			var person = {
+ 				name:"kang",	// "," 分隔不同的属性
+ 				age:"22"
+ 			}
+ 			-使用字面量创建对象时，属性名可以使用字符串
+ 				var person = {
+ 					"name":"kang",
+ 					"age": 22,
+ 					5:true
+ 				}
+ 				--这里的"5"会自动转化字符串；
+ 			-通过对象字面量定义对象时，不会调用 Object 构造函数；
+
+
  5.2 Array 类型
  5.3 Date 类型
  5.4 RegExp 类型
