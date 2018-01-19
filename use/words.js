0b4bf1b86ac05f69d421e89cac466821db729ab1
strayer89 <kangzhitong89@gmail.com>
2018-01-19 23:03:02 +0800

    update

--- use/words.js
+++ use/words.js
@@ -1,18 +1,24 @@
-1.property   ['prɑpɚti]'属性
-2.enumerable [ɪ'njʊmərəbl]'可枚举
-3.anonymous  [ə'nɑnəməs]' 匿名的，无名的
-4.preferred  [prɪ'fɝd]' 首选的
-5.instanceof 实例判断对象类型
-6.dereferencing 解除引用
-7.push 推入
-8.pop 弹出
-9.shift 转移
-10.reverse 颠倒
-11.concat 合并多个数组，合并多个字符串
-12.slice 切割、割破
-13.splice 拼接
-14.split 劈开、分解
-15.configurable [kən'figərəbl]' 可配置的
-16.enumerable [ɪ'njʊmərəbl]' 可列举的 for-in
-17.writable  ['raitəbl]' 可写的
-18.Descriptor [dɪ'skrɪptɚ]' 描述符		Object.getOwnPropertyDescriptor()
+property   ['prɑpɚti]'属性
+enumerable [ɪ'njʊmərəbl]'可枚举
+anonymous  [ə'nɑnəməs]' 匿名的，无名的
+preferred  [prɪ'fɝd]' 首选的
+instanceof 实例判断对象类型
+dereferencing 解除引用
+push 推入
+pop 弹出
+shift 转移
+reverse 颠倒
+concat 合并多个数组，合并多个字符串
+slice 切割、割破
+splice 拼接
+split 劈开、分解
+configurable [kən'figərəbl]' 可配置的
+enumerable [ɪ'njʊmərəbl]' 可列举的 for-in
+writable  ['raitəbl]' 可写的
+Descriptor [dɪ'skrɪptɚ]' 描述符		Object.getOwnPropertyDescriptor()
+excution 执行力，技巧，执行
+scope 范围
+Scope Chain 作用域链
+variable 变量，可变的
+activation 激活
+closure 关闭
